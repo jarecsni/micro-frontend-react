05c1bf03ad5370a8c606b7ad853cc4b2eab86573
Johnny Jarecsni <johnny.jarecsni@icloud.com>
2024-05-24 23:33:45 +0100

    ✨ Update App components in about and home directories

--- about/src/App.tsx
+++ about/src/App.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
 
-import './index.scss'
-
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: about</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+const App: React.FC = () => (
+  <div className='text-3xl mx-auto max-w-6xl'>
+    <div className='text-center'>
+      <img
+        src='https://mdbcdn.b-cdn.net/img/new/avatars/8.webp'
+        className='rounded-full w-32 mb-4 mx-auto'
+        alt='Avatar'
+      />
+      <h5 className='text-xl font-medium leading-tight mb-2'>John Doe</h5>
+      <p className='text-gray-500'>Web designer</p>
+    </div>
   </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+);
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
-
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('app'));
--- home/src/App.tsx
+++ home/src/App.tsx
@@ -1,19 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import { Header } from './Header';
+import { Footer } from './Footer';
 
-import './index.scss'
-
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: TypeScript</div>
-    <div>CSS: Tailwind</div>
+const App: React.FC = () => (
+  <div className='text-3xl mx-auto max-w-6xl'>
+    <Header />
+    <div className='my-10'>
+      Home page Content
+    </div>
+    <Footer />
   </div>
-)
-const rootElement = document.getElementById('app')
-if (!rootElement) throw new Error('Failed to find the root element')
+);
 
-const root = ReactDOM.createRoot(rootElement as HTMLElement)
-
-root.render(<App />)
+ReactDOM.render(<App />, document.getElementById('app'));
